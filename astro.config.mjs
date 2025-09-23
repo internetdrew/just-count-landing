b197fe586abe3b5c1a8277fa63934972cd70eba9
Andrew Rowley <andrew@internetdrew.com>
2025-09-23 13:38:52 -0400

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://justthecount.com",
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
